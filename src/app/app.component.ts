c926eba17cc58fddd5d91361e1829d1154877d68
Ravi Gangawane <melayer.ravi@gmail.com>
2019-02-25 16:02:30 +0530

    add pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-pwa';
+
+  @Input() public pwaPromptEvent = null;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    window.addEventListener('beforeinstallprompt', event => {
+      this.pwaPromptEvent = event;
+      console.log('this.pwaPromptEvent', this.pwaPromptEvent);
+    });
+  }
+  public installPwa(): void {
+    console.log('installPwa...');
+    this.pwaPromptEvent.prompt();
+    this.pwaPromptEvent = null;
+  }
 }
